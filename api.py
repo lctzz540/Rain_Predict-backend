af4e3246c4ac813e64004e6e5f608909402f9b38
Thang Le <lctzz540@outlook.com>
2022-10-25 01:58:56 +0700

    first commit

--- api.py
+++ api.py
@@ -2,11 +2,19 @@ from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+<<<<<<< HEAD
 import joblib
 from sklearn.ensemble import RandomForestClassifier
 
 app = FastAPI()
 loaded_rf = joblib.load("weather_random_forest.joblib")
+=======
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+
+app = FastAPI()
+loaded_rf = pickle.load(open("weather_random_forest.joblib", "rb"))
+>>>>>>> 8a3584b (first commit)
 
 
 class Indicators(BaseModel):
