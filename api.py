8f61e90f2a55a5ab480374d4b505c8a4a00f84f4
Thang Le <lctzz540@outlook.com>
2022-10-27 23:16:07 +0700

    first commit

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sklearn.ensemble import RandomForestClassifier
+import uvicorn
 
 import joblib
 
@@ -33,3 +34,7 @@ def read_root():
 @app.post("/predict")
 def read_item(indicators: Indicators):
     return {"result": int(loaded_rf.predict([[indicators.province, indicators.t_max, indicators.t_min, indicators.wind, indicators.wind_d, indicators.rain_today, indicators.humidi, indicators.cloud, indicators.pressure, indicators.day, indicators.dayofweek, indicators.month]])[0])}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=4000, debug=True)
